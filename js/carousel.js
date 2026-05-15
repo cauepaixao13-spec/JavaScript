84a6843a0cd84853de975135e61aafaf40028e6d
Cuê <cauepaixao13@gmail.com>
2026-05-15 15:59:24 -0300

    Finalizando carousel, adicionando nova imagem e texto.

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,43 @@
-
-
-//carousel
-
-//Array storage class
-let carouselArr = [];
-
-
-//class Carousel
 class Carousel {
 
-    
-      
+    constructor(image, title, url){
+        this.image = image;
+        this.title = title;
+        this.url = url;
+    }
+
     static Start(arr){
         if(arr){
 
             if(arr.length > 0){
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
+                carouselArr = arr;
+
+                Carousel.Next();
+                Carousel._interval = setInterval(function(){ Carousel.Next(); },2000);
             }
-            
+
         } else {
             throw "Method Start need a Array Variable.";
         }
     }
 
     static Next(){
-        
+
+        let carousel = document.getElementById("carousel");
+        let title = document.getElementById("carousel-title");
+
+        let item = carouselArr[Carousel._sequence];
+
+        carousel.style.backgroundImage = "url('" + item.image + "')";
+        title.innerHTML = '<a href="' + item.url + '">' + item.title + '</a>';
+
+        Carousel._sequence++;
+
+        if(Carousel._sequence >= Carousel._size){
+            Carousel._sequence = 0;
+        }
+
     }
-};
+}
